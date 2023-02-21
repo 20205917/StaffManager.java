6a286a15a9ac745506421e7bd1178feff65032b1
GaosongXu <shimaring@qq.com>
2023-02-21 23:32:32 +0800

    finish java doc for StaffID

--- src/StaffID.java
+++ src/StaffID.java
@@ -1,5 +1,15 @@
 import java.util.HashMap;
 
+/**
+ * StaffID is a combination of a letter and a 3-digit number
+ * The letter is a lower case letter from a to z
+ * The number is a 3-digit number from 100 to 999
+ * The letter and number are concatenated to form the ID
+ * The class provides a static method to generate a new StaffID
+ *
+ * @author nil
+ * @version 1.0
+ */
 public class StaffID {
     private final char letter;
     private final String num;
@@ -25,7 +35,17 @@ public class StaffID {
         ID = a + i;
     }
 
-    static public StaffID generateStaffID( HashMap<String, Staff> staffMap){
+    /**
+     * generateStaffID generate a new StaffID
+     *
+     * @param staffMap a HashMap of all the staff
+     *                 the key is the ID, the value is the staff
+     *                 the method will check if the generated ID is already in the HashMap
+     *                 if it is, it will generate a new ID
+     *                 if it is not, it will return the new ID
+     * @return a new StaffID
+     */
+    static public StaffID generateStaffID(HashMap<String, Staff> staffMap) {
         char letter = 'a';
         int num = 0;
         for (char i = 'a'; i <= 'z'; i++) {
