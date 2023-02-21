42779b3852fbfaafbf774b7d067a2216df355676
GaosongXu <shimaring@qq.com>
2023-02-21 22:47:46 +0800

    finish java doc for Researcher

--- src/Researcher.java
+++ src/Researcher.java
@@ -1,32 +1,68 @@
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Researcher class, extends UniversityStaff class
+ * researchers can supervise students’ projects , contains a set of students
+ *
+ * @author nil
+ * @version 1.0
+ * {@inheritDoc}
+ */
 public class Researcher extends UniversityStaff {
 
-    HashSet<Name> students = new HashSet<>();
+    private final HashSet<Name> students = new HashSet<>();
 
     public Researcher(StaffID staffID, SmartCard smartCard) {
         super(staffID, smartCard, Constants.Researcher);
     }
 
+    /**
+     * add student to researcher's student list
+     *
+     * @param name student's name
+     * @throws IllegalArgumentException if name is null
+     */
     public void addStudent(Name name) {
+        if (name == null) {
+            return;
+        }
         students.add(name);
     }
 
+    /**
+     * delete student from researcher's student list
+     *
+     * @param name student's name
+     * @throws IllegalArgumentException if name is null
+     */
     public void deleteStudent(Name name) {
+        if (name == null) {
+            return;
+        }
         students.remove(name);
     }
 
+    /**
+     * delete all students from researcher's student list
+     */
     public void deleteAll() {
         students.clear();
     }
 
-    // check if researcher supervise enough students
+    /**
+     * check if researcher has enough students
+     *
+     * @return true if researcher has enough students
+     * false if researcher has not enough students
+     */
     public boolean isEnough() {
         return students.size() >= 10;
     }
 
-    // list all students researcher supervise
+    /**
+     * get researcher's student list , print all students
+     */
     public void listStudents() {
         for (Name name : students) {
             System.out.println(name);
